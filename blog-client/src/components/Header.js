12d3dcc68827ed36e394faa9b6ff22f84f4c5d50
kkumardeshmukh <kdkrantikumar@gmail.com>
2024-02-18 23:08:47 +0530

    working on front end and navigation bar is created nad working stoped.

---
+++ blog-client/src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <>
+            <nav className="navbar navbar-light bg-light">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">Navbar</a>
+                </div>
+            </nav>
+
+        </>
+    )
+}
+
+export default Header
